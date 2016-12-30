e567d19cdd9c5184fcc700c61c7a7056d4384e69
Josh Ghent <me@joshghent.com>
2016-12-30 21:52:05 +0000

    Added basic routes and test.

---
+++ password.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const randomWords = require('random-words');
+
+function generatePassword() {}
+
+module.exports = generatePassword;
---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = Number(process.env.PORT || 6969);
+const http = require('http').Server(app);
+const api = require('./password.js');
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+});
+
+// Route API calls to the api
+app.use('/api/:length', api);
+
+// Start skynet
+http.listen(port, () => {
+  console.log('Listening on: ' + port);
+});
+
+module.exports = app;
