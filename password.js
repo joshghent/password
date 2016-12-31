3be664cb3d058dce187870610f132e470daad9c9
Josh Ghent <me@joshghent.com>
2016-12-31 02:29:32 +0000

    Added randomize case option.

--- password.js
+++ password.js
@@ -2,9 +2,44 @@
 
 const randomWords = require('random-words');
 
+function randomizeCasing (str) {
+  let newChars = [];
+  for (let i = 0; i < str.length; i += 1) {
+    if (Math.random() > 0.5) {
+      newChars.push(str[i].toUpperCase());
+    } else {
+      newChars.push(str[i]);
+    }
+  }
+
+  return newChars.join('');
+}
+
+function addNumbersToString (str) {
+  return str;
+}
+
+function addSpecialCharsToString (str) {
+  return str;
+}
+
 function generatePassword(req, res) {
+  let password = randomWords({ exactly: Number(req.params.length) }).join('');
+
+  if (req.query.mixCasing === 'true') {
+    password = randomizeCasing(password);
+  }
+
+  if (req.query.includeNumbers === 'true') {
+    password = addNumbersToString(password);
+  }
+
+  if (req.query.includeSpecialChars === 'true') {
+    password = addSpecialCharsToString(password);
+  }
+
   res.json({
-    password: randomWords({ exactly: Number(req.params.length), join: '' })
+    password: password
   });
 }
 
