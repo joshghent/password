3481f42a21a281a1fac59672233768e4f5cc6451
Josh Ghent <me@joshghent.com>
2016-12-31 12:44:08 +0000

    Added include numbers feature.

--- password.js
+++ password.js
@@ -18,7 +18,39 @@ function randomizeCasing (str) {
 }
 
 function addNumbersToString (str) {
-  return str;
+  const commonKeyMap = {
+    'a' : 4,
+    'e' : 3,
+    's' : 5,
+    'z' : 5,
+    'l' : 1,
+    'i' : 1
+  };
+
+  // no more than 10% of letters can be numbers
+  let numbersRemaining = Math.floor(0.10 * str.length);
+
+  let newChars = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    // 50/50 chance of number being swapped for letter
+    if (Math.random() > 0.5 && numbersRemaining > 0) {
+      // If the letter has an equivilent number
+      if (commonKeyMap.hasOwnProperty(str[i].toLowerCase())) {
+        newChars.push(String(commonKeyMap[String(str[i].toLowerCase())]));
+        numbersRemaining -= 1;
+      }
+    } else {
+      newChars.push(str[i]);
+    }
+  }
+
+  // If we have not used all the numbers remaining then append that many numbers to the end of the string
+  if (numbersRemaining > 0) {
+    newChars.push(String(Math.random()).substring(2, (2 + numbersRemaining)));
+  }
+
+  return newChars.join('');
 }
 
 function addSpecialCharsToString (str) {
