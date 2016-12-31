c455e524bbd7901c5a3c602d575029f8a17e3617
Josh Ghent <me@joshghent.com>
2016-12-31 01:06:56 +0000

    Removed console.logs

--- password.js
+++ password.js
@@ -3,9 +3,6 @@
 const randomWords = require('random-words');
 
 function generatePassword(req, res) {
-  console.log(req.params.length, req.length);
-  console.log(randomWords(req.params.length));
-  console.log(randomWords({ exactly: Number(req.params.length) }));
   res.json({
     password: randomWords({ exactly: Number(req.params.length), join: '' })
   });
