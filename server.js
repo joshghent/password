f5c20f677b07b134a5f93c81f8aedbddd6a95e7f
Josh Ghent <me@joshghent.com>
2018-01-31 22:29:13 +0000

    Added scripts for rollbar client side and server side tracking.

--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 
 const express = require('express');
 const path = require('path');
+require('dotenv').config()
 const app = express();
 const port = Number(process.env.PORT || 6969);
 const http = require('http').Server(app);
 const api = require('./password.js');
+const Rollbar = require('rollbar');
+const rollbar = new Rollbar('');
 
 app.use(express.static(path.join(__dirname, 'client')));
+app.use(rollbar.errorHandler());
 
 // Homepage
 app.get('/', (req, res) => {
@@ -19,7 +23,8 @@ app.use('/:length', api);
 
 // Start skynet
 http.listen(port, () => {
-  console.log('Listening on: ' + port);
+	rollbar.info('Test');
+	console.log('Listening on: ' + port);
 });
 
 module.exports = app;
