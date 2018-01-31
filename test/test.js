a5d89031669e0e5d241a1feee7f18907d1316c7e
Josh Ghent <me@joshghent.com>
2018-01-31 23:15:53 +0000

    Fixed up tests.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server.js');
 const should = chai.should();
-const expect = chai.expect();
+const expect = chai.expect;
 const request = require('supertest');
 const assert = require('assert');
 const url = 'http://localhost:' + Number(process.env.PORT || 6969);
@@ -14,10 +14,9 @@ describe('Password API', () => {
   it('Will return a password given a number of words', (done) => {
     request(url)
       .put('/4')
-      .expect('Content-Type', '/json/')
-      .expect(200)
       .end((err, res) => {
-        expect(res.body.password).to.be.a('string');
+				expect(res.body.password).to.be.a('string');
+				expect(res).to.have.status(200)
         done();
       });
   });
