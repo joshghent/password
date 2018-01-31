acd331a98aad8ced4921ab917522b144cc681438
Josh Ghent <me@joshghent.com>
2018-01-31 22:46:21 +0000

    Added helmet for caching. Added emoji for title.

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const api = require('./password.js');
 const Rollbar = require('rollbar');
 const rollbar = new Rollbar(process.env.ROLLBAR_SERVER_SECRET);
 const httpsRedirect = require('express-https-redirect');
+const helmet = require('helmet');
 
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(rollbar.errorHandler());
 
 app.use('/', httpsRedirect());
+app.use(helmet());
 
 // Homepage
 app.get('/', (req, res) => {
